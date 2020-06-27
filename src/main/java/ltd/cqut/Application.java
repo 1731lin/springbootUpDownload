f8766b548624355673914564bb5ce8725e88d86d
林盛斌 <1731976958@qq.com>
2020-06-27 22:20:27 +0800

    2.0

--- src/main/java/ltd/cqut/Application.java
+++ src/main/java/ltd/cqut/Application.java
@@ -1,4 +1,4 @@
-package com.baizhi;
+package ltd.cqut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/ltd/cqut/controller/UpanddownController.java
+++ src/main/java/ltd/cqut/controller/UpanddownController.java
@@ -1,4 +1,4 @@
-package com.baizhi.controller;
+package ltd.cqut.controller;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
